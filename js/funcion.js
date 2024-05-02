d923eed23a94bdd67e8de70c0644aa958dffb860
TelecomNetworkPeru <168131540+TelecomNetworkPeru@users.noreply.github.com>
2024-05-02 14:53:11 -0500

    Update funcion.js

--- js/funcion.js
+++ js/funcion.js
@@ -129,7 +129,26 @@ const loadDataInicio  = ()=>{
     }
 
     document.querySelector("#contenedor").innerHTML = html;
-
 }
 
 loadDataInicio();
+
+function splitTextIntoSpans(target) {
+    let elements = document.querySelectorAll(target)
+    elements.forEach((element) => {
+        element.classList.add('split-text')
+        let text = element.innerText
+        let splitText = text
+            .split(" ")
+            .map(function (word) {
+                let char = word.split('').map(char => {
+                    return `<span class="split-char">${char}</span>`
+                }).join('')
+                return `<div class="split-word">${char}&nbsp</div>`
+            }).join('')
+
+        element.innerHTML = splitText
+    })
+}
+
+splitTextIntoSpans('.bubble-text')
